8f9d4b2d24d4e819b3e839eacaf6ca087fa6b618
SpacePirateG <Vik-dv@yandex.ru>
2016-02-02 20:45:44 +0300

    modified config.js

--- config.js
+++ config.js
@@ -3,16 +3,17 @@ module.exports = {
     likes: /"likes":\{"count":(\d+)/g,
     userId: /com\/(\w+)\//g
   },
-  selectors: {
-    //content :'._8mlbc._t5r8b'; check later     //use paths because id and class generated(check this)
-
-    content : 'div > div > div > a',
-    image : 'div > div > div > div > article > div > div > img',
-    more : 'main > article > div > div >a',
-    scrollTarget : '.ResponsiveBlock'
+  
+  xPathSelectors: {
+    content : '//div/div/div/a[count(*)=1]',
+    image : '//article/div/div/img | //article/div/div/div/img',
+    closeContent : '//div/div/button',
+    more : '//main/article/div/div/a',
+    scrollTarget : '//@[class=".ResponsiveBlock"]'
   },
 
   scrollStep: 200,
-  scrollTimeout: 50
-
+  scrollTimeout: 50,
+  waitElementTimeout: 10000,
+  waitScriptTimeout: 10000
 };
