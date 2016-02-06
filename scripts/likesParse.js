22640fdcc850d8887adba7731dbcce0ce3dcbfd0
SpacePirateG <Vik-dv@yandex.ru>
2016-02-06 17:04:01 +0300

    added likesParse.js

---
+++ scripts/likesParse.js
@@ -0,0 +1,29 @@
+var request = require('request');
+var Promise = require('promise');
+
+var config = require('../crawler/config.js');
+var pattern = config.paterns.likes;
+
+module.exports = function (url) {
+    return new Promise(function (resolve, reject) {
+        request.get(url, function (err, res, body) {
+            try {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                var likesCount = pattern.exec(body)[1];
+				pattern.lastIndex = 0;
+                if (likesCount)
+                    resolve(likesCount);
+                else
+                    reject(new Error('#Not found likes count'));
+            }
+            catch (err) {
+                reject(err);
+            }
+        });
+
+    });
+};
