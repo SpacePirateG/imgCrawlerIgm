31a7ab4436cf8426503e671788541026c974e0e7
SpacePirateG <Vik-dv@yandex.ru>
2016-05-10 23:46:32 +0300

    modify config

--- config.js
+++ config.js
@@ -1,26 +1,42 @@
 module.exports = {
-  paterns: {
-    likes: /"likes":\{"count":(\d+)/g,
-    userId: /com\/(\w+)\//g
-  },
-  //useCss: false,
-
-  xPathSelectors: {
-    name: '//div/div/h1',
-    content: '//a[@class = "_8mlbc _t5r8b" and not(div[@class = "_1lp5e"])]',
-    loading: '//article/div/div/a'
-  },
-  cssSelectors: {
-    nextContent: '.coreSpriteRightPaginationArrow',
-    scrollTarget : '.ResponsiveBlock',
-    image: '._e0mru ._icyx7,._rudo5 ._icyx7',
-    video: '._c8hkj',
-  },
-
-  scrollStep: 200,
-  scrollTimeout: 50,
-  waitElementTimeout: 10000,
-  waitScriptTimeout: 10000,
-  sourceUrl: '',
-  countContent: 2000
+    paterns: {
+        likes: /"likes":\{"count":(\d+)/g,
+        userId: /com\/(\w+)\//g
+    },
+    //useCss: false,
+    xPathSelectors: {
+        name: '//div/div/h1',
+        content: '//a[@class = "_8mlbc _t5r8b" and not(div[@class = "_1lp5e"])]',
+        loading: '//article/div/div/a'
+    },
+    cssSelectors: {
+        nextContent: '.coreSpriteRightPaginationArrow',
+        scrollTarget: '.ResponsiveBlock',
+        image: '._e0mru ._icyx7,._rudo5 ._icyx7',
+        video: '._c8hkj'
+    },
+    profileStates: {
+        free: 'FREE',
+        lock: 'LOCK',
+        done: 'DONE'
+    },
+    module: {
+        name: 'crawler',
+        likes: {
+            name: 'likes',
+            //desc: 'count likes in image',
+            type: 'INT'
+        },
+        profile: {
+            name: 'profile',
+            //desc: 'profile name',
+            type: 'STRING'
+        }
+    },
+    scrollStep: 200,
+    scrollTimeout: 50,
+    waitElementTimeout: 10000,
+    waitScriptTimeout: 10000,
+    sourceUrl: '',
+    countContent: 1000
 };
